6581cca2c5716adfcf28d89b5065efcf1e55bbf0
krishnachaitanyamundluru-lgtm <krishnachaitanyamundluru@gmail.com>
2025-11-29 12:21:54 +0530

    Add files via upload

---
+++ areaofrectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float r,area;
+    float pie = 3.14;
+    printf("Enter radius of a circle");
+    scanf("%f",&r);
+    area = 2*pie*r;
+    printf("AREA OF A CIRCLE IS : %f",area);
+}
